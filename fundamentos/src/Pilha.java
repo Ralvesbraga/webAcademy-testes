4bc825900054fd798b13aae7d2f86d85f4c842c7
Rafael Braga <ralvesbraga40@gmail.com>
2024-11-01 18:25:25 -0500

    feat: fundamento testes pt.2

--- fundamentos/src/Pilha.java
+++ fundamentos/src/Pilha.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
+import java.util.EmptyStackException;
 import java.util.List;
 
 public class Pilha<T> {
     List<T> pilha = new ArrayList<>();
+    int tamanho = 0;
 
+
+    public int size(){
+        return tamanho;
+    }
+
+    public void push(T item){
+        pilha.add(item);
+        tamanho++;
+    }
     
-    
+    public boolean isEmpty(){
+        return (tamanho == 0);
+    }
+
+    public T pop(){
+        if (!this.isEmpty()){
+            T item = pilha.remove(this.tamanho-1);
+            tamanho--;
+            return item;
+        }
+        throw new EmptyStackException();
+    }
 }
--- fundamentos/src/PilhaTest.java
+++ fundamentos/src/PilhaTest.java
@@ -1,29 +1,45 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.util.EmptyStackException;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class PilhaTest {
     // Testar as operações tamanho, vazia, empilhar e desempilhar;
+    Pilha<String> p;
     @Before
-    Pilha p;
     public void Setup(){
-        p = new Pilha();
+        p = new Pilha<>();
     }
 
     @Test
     public void testSize(){
         assertEquals(0, p.size());
-        p.push(item);
+        p.push("1");
         assertEquals(1, p.size());
     }
 
     @Test
-    public void testEmpty(){}
+    public void testEmpty(){
+        assertTrue(p.isEmpty());
+    }
 
     @Test
-    public void testPush(){}
+    public void testPush(){
+        p.push("2");
+        p.push("3");
+        assertEquals(2, p.size());
+    }
 
     @Test
-    public void testPop(){}
+    public void testPop(){
+        p.push("1");
+        p.pop();
+        assertEquals(0, p.size());
+        assertThrows(EmptyStackException.class, () -> p.pop());
+    }
 }
